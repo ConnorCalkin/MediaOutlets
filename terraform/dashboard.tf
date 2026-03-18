9a8a0028bacf26913a570c212efe851ac680b3c8
connorcalkin <connor_calkin@outlook.com>
2026-03-18 14:05:39 +0000

    added logging and changed role for dynamodb access to task role

--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -25,39 +25,71 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_dynamodb_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+#task role for ecs task, which will be used to access dynamodb
+resource "aws_iam_role" "ecs_task_role" {
+  name = "c22-dashboard-divas-ecs-task-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_dynamodb_policy" {
+  role       = aws_iam_role.ecs_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBReadOnlyAccess"
+}
+
+#cloudwatch log group for dashboard service
+resource "aws_cloudwatch_log_group" "dashboard_log_group" {
+    name = "/ecs/c22-dashboard-divas-dashboard-service"
+    retention_in_days = 7
 }
 
 # task definition for dashboard service
 resource "aws_ecs_task_definition" "dashboard-task" {
-  family                   = "c22-dashboard-divas-dashboard-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+    family                   = "c22-dashboard-divas-dashboard-task"
+    network_mode             = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+    cpu                      = "256"
+    memory                   = "512"
+    execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+    task_role_arn            = aws_iam_role.ecs_task_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "c22-dashboard-divas-dashboard-container",
-      image     = "${data.aws_ecr_repository.dashboard-repo.repository_url}:latest",
-      essential = true,
-      portMappings = [
+    container_definitions = jsonencode([
         {
-          containerPort = 80,
-          hostPort      = 80,
-          protocol      = "tcp"
+        name      = "c22-dashboard-divas-dashboard-container",
+        image     = "${data.aws_ecr_repository.dashboard-repo.repository_url}:latest",
+        essential = true,
+        logConfiguration = {
+            logDriver = "awslogs",
+            options = {
+                "awslogs-group"         = aws_cloudwatch_log_group.dashboard_log_group.name,
+                "awslogs-region"        = "eu-west-2",
+                "awslogs-stream-prefix" = "ecs"
+            }
         },
-        {
-          containerPort = 8501,
-          hostPort      = 8501,
-          protocol      = "tcp"
+        portMappings = [
+            {
+            containerPort = 80,
+            hostPort      = 80,
+            protocol      = "tcp"
+            },
+            {
+            containerPort = 8501,
+            hostPort      = 8501,
+            protocol      = "tcp"
+            }
+        ]
         }
-      ]
-    }
-  ])
+    ])
 }
 
 # security group for dashboard service
