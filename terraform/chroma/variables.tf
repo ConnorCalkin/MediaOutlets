1a384b6da73e371ba51ce68b005ffb7137bb2267
zariafk <zariafkhan@gmail.com>
2026-03-18 15:38:55 +0000

    vpc and subnet ids

--- terraform/chroma/variables.tf
+++ terraform/chroma/variables.tf
@@ -1,7 +1,13 @@
 variable "vpc_id" {
     type = string
+    default = "vpc-03f0d39570fbaa750"
 }
 
 variable "private_subnet_ids" {
     type = list(string)
+    default = [
+        "subnet-060cced5eb04380bf",
+        "subnet-0fea0e0a59c4a45ad",
+        "subnet-03d6ad56638c7aa28"
+    ]
 }
