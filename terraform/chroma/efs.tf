ae5692ee17ca7c83850c380cc1da2d4496c44759
zariafk <zariafkhan@gmail.com>
2026-03-18 15:29:08 +0000

    mount target for efs

--- terraform/chroma/efs.tf
+++ terraform/chroma/efs.tf
@@ -6,3 +6,11 @@ resource "aws_efs_file_system" "chroma" {
         Name = "c22-dashboard-divas-chroma-efs"
     }
 }
+
+resource "aws_efs_mount_target" "chroma" {
+    for_each = toset(var.private_subnet_ids)
+
+    file_system_id  = aws_efs_file_system.chroma.id
+    subnet_id       = each.value
+    security_groups = [aws_security_group.chroma_efs.id]
+}
